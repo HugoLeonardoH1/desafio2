2e2934379c9d7dc6098f97b186641e902fb276dc
Hugo Leonardo <60386233+HugoLeonardoH1@users.noreply.github.com>
2024-06-15 21:46:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+const students = [
+  {name: "Luiz",
+    grade1: 8,
+    grade2: 10,
+  },
+   {name: "Juaum",
+    grade1: 3,
+    grade2: 3,
+  },
+   {name: "Dunha",
+    grade1: 9,
+    grade2: 3,
+  },
+   {name: "Zé",
+    grade1: 10,
+    grade2: 3,
+  }
+];
+
+
+function CalcMedia(grade1,grade2){
+ let media = (grade1 + grade2) /2
+ return media
+}
+
+
+function printGrade(student){
+  return `O Estudante ${student.name} foi aprovado com a média ${CalcMedia(student.grade1 , student.grade2)} `
+}
+
+
+    for( let student of students){
+      mediaFinal = CalcMedia(student.grade1 , student.grade2)
+      if (mediaFinal > 7){
+        alert(`O aluno ${student.name} foi aprovado com a média ${mediaFinal}`)
+      }else{
+        alert(`Infelizmente ${student.name} voce não passou, sua média foi ${mediaFinal}`)
+      }
+      }
